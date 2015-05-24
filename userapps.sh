a8da2dcd0ab98a02297d69f36e929c85b712e653
sharjeel <sharjeel.qureshi@gmail.com>
2015-05-24 13:02:38 -0700

    Create userapps.sh

---
+++ userapps.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Run with sudo
+
+which apt-get
+APT_GET=$?
+which pkg
+PKG=$?
+
+if [ $APT_GET -eq 0 ];then
+  apt-get update && sudo apt-get install git python-pip python-dev
+elif  [ $PKG -eq 0 ]; then
+  pkg update
+  pkg install git python-pip python-dev
+fi
+
+pip install ansible
+git clone https://github.com/sharjeel/ansible-playbooks.git
+cd ansible-playbooks
+ansible-playbook -K -i localhost userapps.yml
